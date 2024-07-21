e3cb3f981f442d7a6caccabfa2baca06ab85814e
David Chubabriya <david.chubabriya@gmail.com>
2024-07-21 19:14:52 +0100

    feat(example): add vivs integration example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Extension, Router};
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use vivs::Client;
+
+struct AppState {
+    cache: Client,
+}
+
+type SharedState = Arc<RwLock<AppState>>;
+
+async fn get_handler(Extension(state): Extension<SharedState>) -> String {
+    let mut l = state.write().await;
+    let cache = &mut l.cache;
+    let value = cache.get("name".to_owned()).await;
+    if let Some(cached_value) = value {
+        cached_value
+    } else {
+        "No value".to_owned()
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let vivs_client = Client::new().await;
+    let vivs_client = vivs_client.unwrap();
+
+    let app_state = Arc::new(RwLock::new(AppState { cache: vivs_client }));
+
+    let app = Router::new()
+        .route("/", get(get_handler))
+        .layer(Extension(app_state));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
